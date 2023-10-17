82eed366844d3cfa7c27a0270579853c6494ce60
realRDK <147671485+realRDK@users.noreply.github.com>
2023-10-16 17:27:42 -0700

    Create E.js

---
+++ E.js
@@ -0,0 +1,12 @@
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+  background(220);
+  stroke('black');
+  line(0,0,0,400);
+  line(0,0,200,0);
+  line(0,400,200,400);
+  line(0,200,150,200);
+}
