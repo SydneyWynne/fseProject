920db0150ab61859042909b640cdcd11ffc2c7f0
realRDK <147671485+realRDK@users.noreply.github.com>
2023-10-16 17:28:24 -0700

    A.js

---
+++ A.js
@@ -0,0 +1,12 @@
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+  background(220);
+  fill('black');
+  stroke('black');
+  line(0, 400, 200, 0);
+  line(200, 0, 400, 400);
+  line(100, 200, 300, 200);
+}
