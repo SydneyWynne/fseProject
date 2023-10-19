be980291f72a9f2c8bac91b623f5f8e77d89b76d
alexis-vas <147671739+alexis-vas@users.noreply.github.com>
2023-10-18 17:05:07 -0700

    Create P.js

---
+++ P.js
@@ -0,0 +1,11 @@
+function setup() {
+  createCanvas(400, 400);
+  background(220);
+  strokeWeight(3);
+}
+
+function draw() {
+  noFill();
+  line(150,100,150,300)
+  arc(100,150,250,110,5.5,.8)
+}
