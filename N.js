8188eb2b7806b21da106744d61f0ab47f9b7d2b7
sceron28 <147671346+sceron28@users.noreply.github.com>
2023-10-18 17:09:54 -0700

    Create N.js

---
+++ N.js
@@ -0,0 +1,12 @@
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+  background(220);
+      line(100,100,100,300);
+   
+  line(100,100,200,300);
+   line(200,100,200,300);
+
+}
