fdd5af830838f1249b808141d10991526e5987ba
sceron28 <147671346+sceron28@users.noreply.github.com>
2023-10-18 17:11:08 -0700

    Create R.js

---
+++ R.js
@@ -0,0 +1,11 @@
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+  background(220);
+  noFill();
+  line(150,100,150,300);
+  arc(100,150,250,110,5.5,.8);
+  line(150,200,245,300);
+}
