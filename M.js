cc228b1978efb4053213620d43001e627ff02f8f
sceron28 <147671346+sceron28@users.noreply.github.com>
2023-10-18 17:09:03 -0700

    Create M.js

---
+++ M.js
@@ -0,0 +1,12 @@
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+  background(220);
+  line(100,100,100,300);
+   line(300,100,300,300);
+  line(100,100,200,200);
+   line(300,100,200,200);
+
+}
