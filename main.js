f0257cc30005c6d7628ac19efec872de37a3177b
realRDK <147671485+realRDK@users.noreply.github.com>
2023-10-25 16:48:14 -0700

    Added Reset Button

--- main.js
+++ main.js
@@ -1,6 +1,18 @@
+let button;
+let reset = false;
+
 function setup() {
   createCanvas(400, 400);
   background(220);
+  frameRate(2000);
+  button = createButton("Reset");
+  button.mousePressed(resetSketch);
+}
+
+function resetSketch(){
+  createCanvas(400, 400);
+  background(220);
+  frameRate(2000);
 }
 
 let newLetter = true;
